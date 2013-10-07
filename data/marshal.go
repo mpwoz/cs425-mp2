e7f67757d0d383173f4a8115c980336762c057fc
Martin <martin.woz@gmail.com>
2013-10-06 22:16:16 -0500

    Heartbeat gossiping mostly done.

--- data/marshal.go
+++ data/marshal.go
@@ -1,28 +1,41 @@
 package data
 
 import (
+  "fmt"
+  //"log"
+  "strings"
+  "strconv"
 )
-
 /*
   This will be responsible for the data conversion. It allows us to take an object like 
   GroupMember and change it to an array of bytes to send over UDP. Then, on the other end,
   it should convert these bytes back into the original object. 
 */
 
-
-// The following are just examples to help think about the problem
-// The final implementation may look nothing like this
-
-type MyMarshal struct {
-}
+const (
+  delim = "$$$"
+)
 
 // Serialize a GroupMember for transmission over UDP
 func Marshal(member *GroupMember) (serialized string) {
-  serialized = "TODO, this should be a groupMember's heartbeats"
+  if member == nil {
+    return "NIL"
+  }
+
+  serialized = fmt.Sprintf("%s%s%s", member.Id, delim, member.Heartbeat)
+  //log.Println("Serialized member: %s", serialized)
   return
 }
 
 // Deserialize a transmitted GroupMember
 func Unmarshal(serialized string) (member *GroupMember) {
-  return
+  if serialized == "NIL" {
+    return nil
+  }
+
+  fields := strings.SplitN(serialized, delim, 2)
+  id, hbs := fields[0], fields[1]
+  address := strings.SplitN(id, "###", 2)[0]
+  hb, _ := strconv.Atoi(hbs)
+  return NewGroupMember(id, address, hb)
 }
--- udp/udp.go
+++ udp/udp.go
@@ -8,16 +8,21 @@ package udp
 import (
   "../data"
   "log"
+  "math/rand"
   "net"
-  "strconv"
+  "strings"
   "time"
 )
 
 type Daemon struct {
   Conn *net.UDPConn
+  Port string
+  MemberList map[string]*data.GroupMember
 }
 
-func NewDaemon(hostPort string) (daemon *Daemon, err error) {
+func NewDaemon(port string) (daemon *Daemon, err error) {
+  hostPort := net.JoinHostPort("localhost", port)
+  log.Printf("Creating daemon at %s\n", hostPort)
   conn, err := createUDPListener(hostPort)
 
   if err != nil {
@@ -26,13 +31,15 @@ func NewDaemon(hostPort string) (daemon *Daemon, err error) {
 
   daemon = &Daemon {
     Conn: conn,
+    Port: port,
+    MemberList: make(map[string]*data.GroupMember),
   }
 
   log.Println("Daemon created!")
   return
 }
 
-func (self *Daemon) ReceiveDatagrams() {
+func (self *Daemon) ReceiveDatagrams(joinGroupOnConnection bool) {
   for {
     buffer := make([]byte, 1024)
     c, addr, err := self.Conn.ReadFromUDP(buffer)
@@ -41,39 +48,137 @@ func (self *Daemon) ReceiveDatagrams() {
       return
     }
 
-    message := string(buffer[:c])
+    portmsg := strings.SplitN(string(buffer[:c]), "<PORT>", 2)
+    port, msg := portmsg[0], portmsg[1]
+    senderAddr := net.JoinHostPort(addr.IP.String(), port)
 
-    // Instantiate the new member trying to join the group
-    if message == "JOIN" {
-      senderAddr := net.JoinHostPort(addr.IP.String(), strconv.Itoa(addr.Port))
-      log.Printf("Data received from %s: %s", senderAddr, message)
-      self.addNewMember(senderAddr)
-    }
+    //log.Printf("Data received from %s: %s", senderAddr, msg)
 
+    self.handleMessage(msg, senderAddr, &joinGroupOnConnection)
   }
+}
+
+
+func (self *Daemon) handleMessage(msg, sender string, joinSenderGroup *bool) {
+  fields := strings.SplitN(msg, "|%|", 2)
+  switch fields[0] {
+    case "JOIN":
+      self.addNewMember(sender)
+      if *joinSenderGroup {
+        *joinSenderGroup = false
+        self.JoinGroup(sender)
+      }
+    case "GOSSIP":
+      self.handleGossip(sender, fields[1])
+  }
+}
+
+func (self *Daemon) handleGossip(senderAddr, subject string) {
+  // Reset the counter for the sender
+  // TODO add sender if it doesn't exist yet
+  for id,member := range self.MemberList {
+    if strings.Contains(id, senderAddr) {
+      member.SetHeartBeat(0)
+    }
+  }
+
+  // Update the counter for the subject
+  subjectMember := data.Unmarshal(subject)
+  if subjectMember == nil {
+    return
+  }
+
+  curr := self.MemberList[subjectMember.Id]
+  if curr == nil {
+    self.MemberList[subjectMember.Id] = subjectMember
+  } else {
+    if curr.Heartbeat > subjectMember.Heartbeat {
+      curr.Heartbeat = subjectMember.Heartbeat
+    }
+  }
+
 }
 
 func (self *Daemon) addNewMember(address string) (newMember *data.GroupMember){
   now := time.Now().UTC()
   machineId := address + "###" + now.String()
   newMember = data.NewGroupMember(machineId, address, 0)
-  log.Printf("Created new member with ID: %s", machineId)
-  // TODO add to the list of members
+  log.Printf("Created new member with IP: %s", address)
+  self.MemberList[machineId] = newMember
+  return
+}
+
+func (self *Daemon) JoinGroup(address string) (err error) {
+  return self.sendMessageWithPort("JOIN", address)
+}
+
+func (self *Daemon) Gossip(subject,receiver *data.GroupMember) (err error) {
+  // The message we are sending over UDP, subject can be nil
+  msg := "GOSSIP|%|" + data.Marshal(subject)
+  return self.sendMessageWithPort(msg, receiver.Address)
+}
+
+func (self *Daemon) sendMessageWithPort(msg, address string) (err error) {
+  msg = self.Port + "<PORT>" + msg
+  return sendMessage(msg, address)
+}
+
+
+// Increment all heartbeat values, and gossip a random machine A to another random B
+func (self *Daemon) HeartbeatAndGossip() {
+  // Nobody in the list yet
+  if len(self.MemberList) < 1 {
+    return
+  }
+
+  // There is only one other machine, send them a dummy message
+  if len(self.MemberList) == 1 {
+    // is this the only way to get the first element of a map?
+    for _, receiver := range self.MemberList {
+      self.Gossip(nil, receiver)
+    }
+    return
+  }
+
+  receiverIndex := rand.Int() % len(self.MemberList)
+  var receiver *data.GroupMember
+  i := 0
+  for _, currMember := range self.MemberList {
+    if receiverIndex == i { receiver = currMember }
+    currMember.IncrementHeartBeat()
+    i++
+  }
+  for _, subject := range self.MemberList {
+    if subject.Id == receiver.Id {
+      continue
+    }
+    self.Gossip(subject, receiver)
+  }
+}
+
+
+// Get two random numbers that aren't the same
+// TODO there's probably a better (quicker) way to do this 
+func distinctRandoms(max int) (a, b int) {
+  a = rand.Int() % max
+  for b = a; b == a; {
+    b = rand.Int() % max
+  }
   return
 }
 
 
-func SendMessage(message, address string) (err error) {
-  var udpaddr *net.UDPAddr
-  if udpaddr, err = net.ResolveUDPAddr("udp4", address); err != nil {
+func sendMessage(message, address string) (err error) {
+  var raddr *net.UDPAddr
+  if raddr, err = net.ResolveUDPAddr("udp", address); err != nil {
     log.Panic(err)
   }
 
-  var conn *net.UDPConn
-  conn, err = net.DialUDP("udp4", nil, udpaddr)
-  log.Printf("Sending '%s' to %s..", message, udpaddr)
-  if _, err = conn.Write([]byte(message)); err != nil {
-    log.Panic("Writing to UDP", err)
+  var con *net.UDPConn
+  con, err = net.DialUDP("udp", nil, raddr)
+  //log.Printf("Sending '%s' to %s..", message, raddr)
+  if _, err = con.Write([]byte(message)); err != nil {
+    log.Panic("Writing to UDP:", err)
   }
 
   return
@@ -83,11 +188,11 @@ func SendMessage(message, address string) (err error) {
 func createUDPListener(hostPort string) (conn *net.UDPConn, err error) {
 
   var udpaddr *net.UDPAddr
-  if udpaddr, err = net.ResolveUDPAddr("udp4", hostPort); err != nil {
+  if udpaddr, err = net.ResolveUDPAddr("udp", hostPort); err != nil {
     return
   }
 
-  conn, err = net.ListenUDP("udp4", udpaddr)
+  conn, err = net.ListenUDP("udp", udpaddr)
 
   return
 }
